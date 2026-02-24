3bb8765b898dd1ac1334a9095ae1d35fee6fcef8
GibsonK <gibsonk@RCMAC25.local>
2026-02-24 14:46:08 +1100

    unix sockets

--- client/main.cxx
+++ client/main.cxx
@@ -1,9 +1,62 @@
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+#include <cstring>
 #include <iostream>
-#include "connection.h"
+
+class ClientConnection {
+ public:
+  void connect() {
+    sock_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sock_fd < 0) {
+      throw std::runtime_error("Could not acquire socket file descriptor");
+    }
+
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, SOCKET_PATH, sizeof(addr.sun_path) - 1);
+
+    if (::connect(sock_fd, (sockaddr*)&addr, sizeof(addr)) < 0) {
+      throw std::runtime_error("Could not connect to socket");
+    }
+  }
+
+  std::string receive() {
+    char buf[100];
+    ssize_t n = read(sock_fd, buf, sizeof(buf) - 1);
+    if (n > 0) {
+      buf[n] = '\0';
+      return std::string(buf);
+    }
+
+    return "";
+  }
+
+  void send(const std::string& message) { write(sock_fd, message.data(), message.size()); }
+
+  ~ClientConnection() { close(sock_fd); }
+
+ private:
+  int sock_fd = 0;
+  sockaddr_un addr{};
+  const char* SOCKET_PATH = "/tmp/messager.sock";
+};
 
 int main() {
-  auto conn = Connection();
-  
-  conn.print();
-  std::cout << "testing\n";
+  auto conn = ClientConnection();
+
+  try {
+    conn.connect();
+  } catch (std::exception& e) {
+    std::cerr << "Error connecting to socket, details: " << e.what() << '\n';
+    return EXIT_FAILURE;
+  }
+
+  conn.send("Hello from the client!");
+
+  if (auto message = conn.receive(); message.size() != 0) {
+    std::cout << "received: " << message << " back from the server\n";
+  }
+
+  return 0;
 }
--- server/main.cxx
+++ server/main.cxx
@@ -1,9 +1,79 @@
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+#include <cstring>
+#include <expected>
 #include <iostream>
-#include "connection.h"
+
+class ServerConnection {
+ public:
+  void connect() {
+    this->server_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (this->server_fd < 0) {
+      throw std::runtime_error("Could not acquire socket file descriptor");
+    }
+
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, SOCKET_PATH, sizeof(addr.sun_path) - 1);
+
+    // remove existing socket
+    unlink(SOCKET_PATH);
+
+    if (bind(server_fd, (sockaddr*)&addr, sizeof(addr)) < 0) {
+      throw std::runtime_error("Could not bind to socket");
+    }
+
+    if (listen(server_fd, 5) < 0) {
+      throw std::runtime_error("Could not listen to socket");
+    }
+
+    client_fd = accept(server_fd, nullptr, nullptr);
+    if (client_fd < 0) {
+      throw std::runtime_error("Accept on socket failed");
+    }
+  }
+
+  std::string receive() {
+    char buffer[100];
+    ssize_t n = read(client_fd, buffer, sizeof(buffer) - 1);
+    if (n > 0) {
+      buffer[n] = '\0';
+      return std::string(buffer);
+    }
+
+    return "";
+  }
+
+  void send(const std::string& message) { write(client_fd, message.data(), message.size()); }
+
+  ~ServerConnection() {
+    close(client_fd);
+    close(server_fd);
+    unlink(SOCKET_PATH);
+  }
+
+ private:
+  int server_fd = 0;
+  int client_fd = 0;
+  sockaddr_un addr{};
+  const char* SOCKET_PATH = "/tmp/messager.sock";
+};
 
 int main() {
-  auto conn = Connection();
-  
-  conn.print();
-  std::cout << "testing\n";
+  auto conn = ServerConnection();
+
+  try {
+    conn.connect();
+  } catch (std::exception& e) {
+    std::cerr << "Error connecting to socket, details: " << e.what() << '\n';
+    return EXIT_FAILURE;
+  }
+
+  if (auto data = conn.receive(); data.size() != 0) {
+    std::clog << "got: " << data << "\n";
+    conn.send("Got your message:)");
+  }
+
+  return EXIT_SUCCESS;
 }
